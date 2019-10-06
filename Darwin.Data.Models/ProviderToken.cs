876d2b0a394347796bc76e1d35e0a22915bc0b59
Manvendra Kumar <manvendra.kumar7@gmail.com>
2019-10-06 19:37:43 +0530

    Add project files.

---
+++ Darwin.Data.Models/ProviderToken.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Darwin.Data.Models
+{
+    public class ProviderToken
+    {
+        public string IdToken { get; set; }
+    }
+}
---
+++ Darwin.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Darwin.Data.Models;
+using Google.Apis.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace Darwin.WebApi.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Booyah!!!";
+        }
+
+        [AllowAnonymous]
+        [HttpPost("google")]
+        public async Task<IActionResult> Google([FromBody]ProviderToken providerToken)
+        {
+            try
+            {
+                var payload = await GoogleJsonWebSignature.ValidateAsync(providerToken.IdToken, new GoogleJsonWebSignature.ValidationSettings
+                {
+                    Audience = new List<string>
+                    {
+                        configuration["AppSettings:GoogleClientId"]
+                    }
+                });
+
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Email, payload.Email),
+                    new Claim(JwtRegisteredClaimNames.Sub, payload.Subject),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration["AppSettings:JwtSecret"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(string.Empty,
+                  string.Empty,
+                  claims,
+                  expires: DateTime.Now.AddSeconds(55 * 60),
+                  signingCredentials: creds);
+
+                return Ok(
+                    new
+                    {
+                        token = new JwtSecurityTokenHandler().WriteToken(token)
+                    });
+            }
+            catch(Exception ex)
+            {
+                BadRequest(ex.Message);
+            }
+
+            return BadRequest();
+        }
+    }
+}
